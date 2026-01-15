b7c14c052f7dc55c99c342d6fc3e2c39177f3d1e
Shwetakankanwadi <shwetakankanwadi878@gmail.com>
2026-01-15 22:06:28 +0530

    Update test_temp.py

--- test_temp.py
+++ test_temp.py
@@ -1,13 +1,10 @@
 from temp import celsius_to_fahrenheit
 
-
 def test_freezing_point():
     assert celsius_to_fahrenheit(0) == 32
-
 
 def test_boiling_point():
     assert celsius_to_fahrenheit(100) == 212
 
-
-def test_negative_temperature():
+def test_negative():
     assert celsius_to_fahrenheit(-40) == -40
