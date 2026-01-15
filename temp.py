80c6e0de2640b3e9bf044d03cf0a2322c477f2c9
Shwetakankanwadi <shwetakankanwadi878@gmail.com>
2026-01-15 22:06:44 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,9 +1,15 @@
 def celsius_to_fahrenheit(celsius):
-
     return (celsius * 9 / 5) + 32
 
 
+def main():
+    try:
+        temp_c = float(input("Enter temperature in Celsius: "))
+        temp_f = celsius_to_fahrenheit(temp_c)
+        print("Temperature in Fahrenheit:", temp_f)
+    except ValueError:
+        print("Please enter a valid number.")
+
+
 if __name__ == "__main__":
-    temp_c = float(input("Enter temperature in Celsius:"))
-    temp_f = celsius_to_fahrenheit(temp_c)
-    print("Temperature in Fahrenheit:",temp_f)
+    main()
